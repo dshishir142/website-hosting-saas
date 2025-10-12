e85df79f9a666b408730de34eb8ac5dcbbe561e5
dshishir142 <np03cs4a220383@heraldcollege.edu.np>
2025-10-12 12:50:40 +0545

    login inputs

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React, { useState } from "react";
+
+type LoginForm = {
+    name: string;
+    password: string;
+};
+
+export default function Login() {
+
+    const [ user, setUser ] = useState<LoginForm>({
+        name: "",
+        password: "",
+    })
+
+    const submit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(`${user.name} and ${user.password}`);
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setUser((prev) => ({...prev, [name] : value}));
+    }
+
+    return (
+        <div className="flex h-screen w-screen items-center justify-center">
+            <form onSubmit={submit}>
+
+                <div className="flex flex-col h-[300px] w-[300px] justify-center bg-gray-700 rounded-4xl space-y-3 items-center">
+
+                    <input className=" h-[40px] p-[10px] rounded-3xl bg-gray-500"
+                    name="name"
+                    value={user.name}
+                    onChange={handleChange}
+                        placeholder="username" />
+
+                    <input className=" h-[40px] p-[10px] rounded-3xl bg-gray-500"
+                    name="password"
+                    value={user.password}
+                    onChange={handleChange}
+                    type="password"
+                        placeholder="password" />
+
+                    <button className=" border-1 h-[40px] w-[150px] rounded-4xl hover:bg-gray-400">Login</button>
+
+                </div>
+            </form>
+        </div>
+    )
+}
