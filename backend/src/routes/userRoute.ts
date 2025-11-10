b10ca6ab4698b0e3e14ca5b2b015bdef3d557d68
dshishir142 <np03cs4a220383@heraldcollege.edu.np>
2025-11-10 13:37:51 +0545

    I hate prisma

--- backend/src/routes/userRoute.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { getUsers } from '../controllers/userController';
-
-router.get('/getUsers', getUsers);
-
-export default router;
