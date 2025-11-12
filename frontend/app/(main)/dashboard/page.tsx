5f7535687f6bb6ecbd3c4f0caea8342a54dfcb30
dshishir142 <np03cs4a220383@heraldcollege.edu.np>
2025-11-12 14:43:01 +0545

    started working on dashboard

--- frontend/app/(main)/dashboard/page.tsx
+++ frontend/app/(main)/dashboard/page.tsx
@@ -1,7 +1,32 @@
 "use client";
-export default function Page(){
-    return(
+
+import { useContext } from "react";
+
+export default function Page() {
+
+    const localData = JSON.parse(localStorage.getItem('user') || "{}");
+
+
+    const handleChange = ()=>{
+
+    }
+
+    return (
         <>
-        This is home Page</>
+            <div className="flex items-center justify-center">
+                {localData.subdomain != null ? (
+                    <>
+                        <div>Choose subdomain</div>
+                        <input className=" h-[40px] p-[10px] rounded-3xl bg-gray-500"
+                            name="subdomain"
+                            // value={subdomain}
+                            onChange={handleChange}
+                            placeholder="subdomain" />
+                    </>
+                ) : (
+                    <>{localData.subdomain}</>
+                )}
+            </div>
+        </>
     )
 }
