0e4db6815efc2f85be67d7c2962ad55ece101e71
dshishir142 <np03cs4a220383@heraldcollege.edu.np>
2025-12-11 19:10:17 +0545

    added data fetching in tenants site will separate subdomain next day

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,14 +1,5 @@
-"use client";
-import { useEffect, useState } from "react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-    const [url, setUrl] = useState('');
-    useEffect(()=>{
-      setUrl(window.location.href);
-    })
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      {url}
-    </div>
-  );
+  redirect('/dashboard');
 }
