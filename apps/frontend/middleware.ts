7e046945f6241dd6939d74c41ac71da501dc920d
dshishir142 <np03cs4a220383@heraldcollege.edu.np>
2025-12-12 19:47:41 +0545

    need to use cookies for auth handeling or nextauth

--- apps/frontend/middleware.ts
+++ apps/frontend/middleware.ts
@@ -2,7 +2,23 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
+  const url = req.nextUrl.clone();
+  const pathName = req.nextUrl.pathname;
 
+  const free = ['/signup', '/login']
 
-  return NextResponse.next();
+  if(free.includes(pathName)){
+    console.log("Didnt open other");
+    return NextResponse.next();
+  }
+
+  console.log(`opened ${pathName}`)
+  url.pathname = '/login';
+  return NextResponse.redirect(`${url}`);
 }
+
+export const config = {
+  matcher: [
+    "/((?!_next|favicon.ico).*)"
+  ]
+};
--- backend/src/jwtmiddleware.ts
+++ backend/src/jwtmiddleware.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
-import { env } from 'process';
 
 export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization'];
